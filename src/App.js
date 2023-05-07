8cf773cf8d14b471a35901dece68a07a424ba014
Parthakmehra <parthakmehra3@gmail.com>
2023-05-07 15:25:57 +0530

    App.js and App.css changed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [mobile, setMobile] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleMobileChange = (event) => {
+    setMobile(event.target.value);
+  };
+
+  const handleGenerateOTP = async () => {
+    try {
+      const response = await axios.post('https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP', {
+        mobile: parseInt(mobile),
+      });
+
+      setErrorMessage('');
+      setMobile('');
+    } catch (error) {
+      console.error(error);
+      if (error.response && error.response.data && error.response.data.error) {
+        setErrorMessage(error.response.data.error);
+      } else {
+        setErrorMessage('An error occurred while generating OTP.');
+      }
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Generate OTP</h1>
+      <p>Enter your 10-digit mobile number:</p>
+      <input type="tel" value={mobile} onChange={handleMobileChange} pattern="[0-9]{10}" required />
+      <button onClick={handleGenerateOTP}>Generate OTP</button>
+      {errorMessage && <p className="error">{errorMessage}</p>}
     </div>
   );
 }
